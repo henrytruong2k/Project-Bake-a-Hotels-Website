4e6e7d69020a3c0e8f937ebd7ada37314ff36d26
Quang Huy <kzyo5962@gmail.com>
2020-08-24 15:15:08 +0700

    fix debug

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-jQuery(document).ready(function ( $ ) {
-    $('.counter').counterUp({
-    delay: 10,
-    time: 1000
-    });
-})
-
-
+// jQuery(document).ready(function( $ ) {
+//     $('.counter').counterUp({
+//     delay: 10,
+//     time: 1000
+//     });
+// })
+$('.counter').counterUp();
 const toTop = document.querySelector('.to-top');
 window.addEventListener('scroll', () => {
   if(window.pageYOffset > 100) {
     toTop.classList.add("active-to-top");
-  } else {
+  } 
+  else {
     toTop.classList.remove("active-to-top");
   }
 })
@@ -32,3 +32,37 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+//set scroll in bottom page
+window.addEventListener('scroll', () => {
+  const scrolled = window.scrollY;
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  if(Math.ceil(scrolled) === scrollable) {
+    alert('You\'re in the bottom page');
+  }
+  
+  
+  console.log(scrolled);
+});
+
+// const counters = document.querySelectorAll('.counter');
+// const speed = 10;
+
+
+// counters.forEach(counter => {
+//   const updateCount = () => {
+//     const target = counter.getAttribute('data-target');
+//     const count = +counter.innerText;
+//     //console.log('count: ' + count);
+//     const inc = target / speed;
+    
+//     // console.log(inc);
+//     if(count < target) {
+//       counter.innerText = count + Math.trunc(inc);
+//       setTimeout(updateCount, 100);
+//     } else {
+//       count.innerText = target;
+//     }
+//     //console.log(typeof target);
+//   }
+//   updateCount();
+// });
